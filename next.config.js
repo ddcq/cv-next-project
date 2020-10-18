6d081e636471766685f81ace44bc2994a27a7e5b
Denis DECLERCQ <ddeclercq@evenium.us>
2020-10-18 16:44:41 +0200

    build: windows compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 
 module.exports = () => {
-	if (process.env.NODE_ENV !== 'development') {
+	if (process.env.NODE_ENV !== 'development' && process.env.NODE_ENV !== 'dev') {
 		// Config used to run in production.
 		return {
 			assetPrefix: '/cv-next-project/',
