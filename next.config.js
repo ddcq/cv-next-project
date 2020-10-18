cb9f0878051f695621c6be11fa660a777c437f22
Denis DECLERCQ <denis.declercq@evenium.com>
2020-10-18 13:31:49 +0200

    fix local next config raise 404

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
-module.exports = {
-  assetPrefix: "/cv-next-project/",
-  basePath: "/cv-next-project",
+/* eslint-disable no-undef */
+const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development' ? {} : require('next/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+	if (phase === PHASE_PRODUCTION_SERVER) {
+		// Config used to run in production.
+		return {
+			assetPrefix: '/cv-next-project/',
+			basePath: '/cv-next-project',
+		};
+	}
+	return {};
 };
