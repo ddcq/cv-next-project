f12f4539a58862ee4e055adda2bb026c4c63299e
Denis DECLERCQ <ddeclercq@evenium.us>
2020-10-18 13:57:31 +0200

    fix: prod config next

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-undef */
-const { PHASE_PRODUCTION_SERVER } = process.env.NODE_ENV === 'development' ? {} : require('next/constants');
 
-module.exports = (phase, { defaultConfig }) => {
-	if (phase === PHASE_PRODUCTION_SERVER) {
+module.exports = () => {
+	if (process.env.NODE_ENV !== 'development') {
 		// Config used to run in production.
 		return {
 			assetPrefix: '/cv-next-project/',
